10d869410409a2d112559f24f7a001ac7bfb2f80
Brijesh Kumar Singh <brij10718@gmail.com>
2020-09-10 12:56:01 +0530

    Completed component modules

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,24 +1,36 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {IProduct} from "./product"
 
 @Component({
     selector: "pm-products",
-    templateUrl: "./product-list.component.html"
+    templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list.component.css"]
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List!';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
+    filteredProducts: IProduct[] ;
+
+    _listFilter: string;
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string): {
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
             "productCode": "GDN-0011",
             "releaseDate": "March 19, 2019",
             "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
+            "price": 119.95,
             "starRating": 3.2,
             "imageUrl": "assets/images/leaf_rake.png"
           },
@@ -28,7 +40,7 @@ export class ProductListComponent{
             "productCode": "GDN-0023",
             "releaseDate": "March 18, 2019",
             "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
+            "price": 132.99,
             "starRating": 4.2,
             "imageUrl": "assets/images/garden_cart.png"
           },
@@ -38,7 +50,7 @@ export class ProductListComponent{
             "productCode": "TBX-0048",
             "releaseDate": "May 21, 2019",
             "description": "Curved claw steel hammer",
-            "price": 8.9,
+            "price": 89.9,
             "starRating": 4.8,
             "imageUrl": "assets/images/hammer.png"
           },
@@ -48,7 +60,7 @@ export class ProductListComponent{
             "productCode": "TBX-0022",
             "releaseDate": "May 15, 2019",
             "description": "15-inch steel blade hand saw",
-            "price": 11.55,
+            "price": 101.55,
             "starRating": 3.7,
             "imageUrl": "assets/images/saw.png"
           },
@@ -58,14 +70,31 @@ export class ProductListComponent{
             "productCode": "GMG-0042",
             "releaseDate": "October 15, 2018",
             "description": "Standard two-button video game controller",
-            "price": 35.95,
+            "price": 335.95,
             "starRating": 4.6,
             "imageUrl": "assets/images/xbox-controller.png"
           }
-    ]
+    ];
+
+    constructor (){
+      this.filteredProducts = this.products;
+      // this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[]{
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) => 
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1)
+
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage
+    }
+
+    ngOnInit(): void {
+        console.log(`In OnInit`);
+        
     }
 
 }
